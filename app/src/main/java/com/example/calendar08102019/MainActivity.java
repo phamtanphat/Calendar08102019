59b2efe4a9827bc19704613bbc04481858dc7bfb
phatdroid94 <phatdroid94@gmail.com>
2019-11-21 20:10:30 +0700

    calendar

--- app/src/main/java/com/example/calendar08102019/MainActivity.java
+++ app/src/main/java/com/example/calendar08102019/MainActivity.java
@@ -3,14 +3,26 @@ package com.example.calendar08102019;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+//        Date date = new Date();
+//        long currentTime = System.currentTimeMillis();
+        Calendar calendar = Calendar.getInstance();
+//        SimpleDateFormat simpleDateFormat = new SimpleDateFormat("YYYY-MM-dd");
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+        int month = calendar.get(Calendar.MONTH);
+        int year = calendar.get(Calendar.YEAR);
+        Log.d("BBB",year + "");
+
     }
-    encrypt
-    decrypt
 }
